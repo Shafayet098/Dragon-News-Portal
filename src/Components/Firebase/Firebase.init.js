918c49a7d922c5819646b015f5495a58f6a91901
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-03-14 00:47:10 +0600

    hide-credentials-of-firebase

--- src/Components/Firebase/Firebase.init.js
+++ src/Components/Firebase/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfxY9ZeEXY1FBTmZm90gSNttGgG3OprqU",
-  authDomain: "dragon-news-portal-aa128.firebaseapp.com",
-  projectId: "dragon-news-portal-aa128",
-  storageBucket: "dragon-news-portal-aa128.firebasestorage.app",
-  messagingSenderId: "469262407497",
-  appId: "1:469262407497:web:06bd99eec886ac96fa6c98"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain ,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
